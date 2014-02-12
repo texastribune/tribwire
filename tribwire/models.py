f8489225fb39872c7487566d0c072540e04baaf2
Shelby Wilson <s.wilson024@gmail.com>
2014-02-12 11:25:53 -0600

    update models

--- tribwire/models.py
+++ tribwire/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib import admin
 
 class Link(models.Model):
-	headline = models.CharField(max_length=128, unique=True)
-	blurb = models.CharField(max_length=128, unique=True)
-	dateSuggested = models.DateField() 
+	headline = models.CharField(max_length=128)
+	blurb = models.CharField(max_length=128)
+	date_suggested = models.DateField() 
 	#user = models.OnetoOneField(User)
-	#source = models.OnetoOneField(Source)
-	#wire = models.ForeignKey(Wire)
+	source = models.ForeignKey('Source')
+	wire = models.ForeignKey('Wire')
 
 class Source(models.Model):
-	source = models.CharField(max_length=128, unique=False)
-	link = models.URLField()
+	name = models.CharField(max_length=128, unique=True)
+	url = models.URLField()
 	#favicon = models.OnetoOneField(Favicon)
 
 class Wire(models.Model):
 	name = models.CharField(max_length=128, unique=True)
+	slug = models.SlugField()
