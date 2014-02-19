dc48f7c87beef653001da82a41ac92b29d3913ae
Shelby Wilson <s.wilson024@gmail.com>
2014-02-19 11:57:48 -0600

    update Link modelform- submit button saves data

--- tribwire/views.py
+++ tribwire/views.py
@@ -19,4 +19,7 @@ def index(request):
 
 def new_wire(request):
     form = PostModelForm(request.POST or None)
+    if form.is_valid():
+    	save_it=form.save(commit=False)
+    	save_it.save()
     return render_to_response('tribwire/new_wire.html', locals(), context_instance=RequestContext(request))
