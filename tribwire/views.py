20da20994ffe65eb79a4ff6f391e6edfe0b77e05
Shelby Wilson <s.wilson024@gmail.com>
2014-02-21 15:27:41 -0600

    add basic form

--- tribwire/views.py
+++ tribwire/views.py
@@ -4,22 +4,14 @@ from django.http import HttpResponse
 from .forms import PostModelForm
 
 def index(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
-    context = RequestContext(request)
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    return render_to_response('tribwire/index.html', context_dict, context)
+    return render_to_response('tribwire/index.html', locals(), context_instance=RequestContext(request))
 
 def new_wire(request):
+
     form = PostModelForm(request.POST or None)
     if form.is_valid():
     	save_it=form.save(commit=False)
     	save_it.save()
+    	
     return render_to_response('tribwire/new_wire.html', locals(), context_instance=RequestContext(request))
