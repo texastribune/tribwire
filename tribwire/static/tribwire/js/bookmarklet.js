d376eafc7d88cb74c158ef2441468164eec9103f
Shelby Wilson <s.wilson024@gmail.com>
2014-03-12 15:46:28 -0500

    move bookmarklet.js

---
+++ tribwire/static/tribwire/js/bookmarklet.js
@@ -0,0 +1,66 @@
+//(function(){
+  var 
+      method = {},
+      $overlay,
+      $modal,
+      $content,
+      $close;
+
+  // Horizontally center the modal in the viewport
+  method.center = function () {
+    var top, left;
+
+    top = Math.max($modal.outerHeight() - $(window).height(), 0);
+    left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+
+    $modal.css({
+      top:top + $(window).scrollTop(), 
+      left:left + $(window).scrollLeft()
+    });
+  };
+
+  // Open the modal
+  method.open = function (settings) {
+    $content.empty().append(settings.content);
+
+    $modal.css({
+        width: settings.width || 'auto', 
+        height: settings.height || 'auto'
+    });
+
+    method.center();
+    $(window).bind('resize.modal', method.center);
+    $modal.show();
+    $overlay.show();
+  };
+
+  // Close the modal
+  method.close = function () {
+    $modal.hide();
+    $overlay.hide();
+    $content.empty();
+    $(window).unbind('resize.modal');
+  };
+
+  // Generate the HTML and add it to the document
+  $overlay = $('<div id="overlay"></div>');
+  $modal = $('<div id="modal"></div>');
+  $content = $('<div id="content"></div>');
+  $close = $('<a id="close" href="#">close</a>');
+
+  $modal.hide();
+  $overlay.hide();
+  $modal.append($content, $close);
+
+  $close.click(function(e){
+    e.preventDefault();
+    method.close();
+  });
+
+
+  $('body').append($overlay, $modal);                       
+  // Wait until the DOM has loaded before querying the document
+  $.get('new_wire.html', function(data){
+    method.open({content: data});
+  }); 
+//})();
