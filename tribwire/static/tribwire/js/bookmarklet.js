fe51ac6aeeddc897a884050a4f391462fb03dc0a
Shelby Wilson <s.wilson024@gmail.com>
2014-03-12 15:54:17 -0500

    add jquery check

--- tribwire/static/tribwire/js/bookmarklet.js
+++ tribwire/static/tribwire/js/bookmarklet.js
@@ -1,4 +1,23 @@
-//(function(){
+
+  var v = "1.3.2";
+
+  if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+    var done = false;
+    var script = document.createElement("script");
+    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+    script.onload = script.onreadystatechange = function(){
+      if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+        done = true;
+        initMyBookmarklet();
+      }
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+  } else {
+    initMyBookmarklet();
+  }
+
+
+function initMyBookmarklet(){
   var 
       method = {},
       $overlay,
@@ -63,4 +82,4 @@
   $.get('new_wire.html', function(data){
     method.open({content: data});
   }); 
-//})();
+}
