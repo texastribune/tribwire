0e53893527f63e16e9398d18f1d19578bc14334a
Shelby Wilson <s.wilson024@gmail.com>
2014-03-19 14:01:40 -0500

    add trailing comma

--- tribwire/urls.py
+++ tribwire/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = patterns('',
 		url(r'^$', views.index, name='index'),
-		url(r'^link_form.html$', views.CreateLink.as_view(), name='link_form'))
+		url(r'^link_form.html$', views.CreateLink.as_view(), name='link_form'),
+		)
