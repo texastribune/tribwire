bcc9e25e346e77e40b33d55c62d80ea8b98f32ea
crccheck <c@crccheck.com>
2014-06-18 20:54:50 -0500

    remove baggage leftover from the template project

--- example/settings.py
+++ example/settings.py
@@ -2,9 +2,10 @@
 
 # Setup a ``project_dir`` function
 import os
-from dj_settings_helpers import create_project_dir
-project_dir = create_project_dir(os.path.join(os.path.dirname(__file__),
-        '..', '..'))
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(__file__)
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -17,7 +18,7 @@ MANAGERS = ADMINS
 
 import dj_database_url
 DATABASE_URL = os.environ.get('DATABASE_URL',
-        'sqlite:///%s' % project_dir('project.db'))
+    'sqlite:///%s' % os.path.join(BASE_DIR, 'tribwire.db'))
 DATABASES = {'default': dj_database_url.parse(DATABASE_URL), }
 
 # Local time zone for this installation. Choices can be found here:
@@ -74,7 +75,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -84,7 +85,7 @@ SECRET_KEY = 'drlfrd0yo@x^b7z(bnae5q=3bo(od!#5nsm0%l@-^9y=3l@9cu'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -97,16 +98,14 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'project.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    project_dir('templates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
 
 INSTALLED_APPS = (
@@ -124,36 +123,6 @@ INSTALLED_APPS = (
     # Custom application being tested
     'tribwire',
 
-    # The app with all of the tests and any example customizations
-    'app',
+    # Extras
     'django_extensions',
 )
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
