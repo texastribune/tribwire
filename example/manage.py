8c8afe5ec6728f42d5d694a684d6b25d9bdb8648
Kathryn Beaty <kabeaty@gmail.com>
2014-07-17 15:16:23 -0500

    Add comments to bookmarklet.js to learn what's happening here and document it

--- example/manage.py
+++ example/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
 
--- tribwire/static/tribwire/js/bookmarklet.js
+++ tribwire/static/tribwire/js/bookmarklet.js
@@ -1,7 +1,11 @@
-//(function (){
+(function (){
 
+  //Set jQuery version needed
   var v = "1.7.2";
 
+  //Check to see if page already has jQuery of correct version loaded
+  //If it doesn't, load jQuery
+  //Once jQuery is known to be loaded, initialize bookmarklet
   if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
     var done = false;
     var script = document.createElement("script");
@@ -12,6 +16,7 @@
         initMyBookmarklet();
       }
     };
+    //Add jQuery script to the HTML head section
     document.getElementsByTagName("head")[0].appendChild(script);
   } else {
     initMyBookmarklet();
@@ -19,7 +24,7 @@
 
 
   function initMyBookmarklet(){
-    var 
+    var
         method = {},
         $modal,
         $overlay,
@@ -34,17 +39,18 @@
       left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
 
       $modal.css({
-        top:top + $(window).scrollTop(), 
+        top:top + $(window).scrollTop(),
         left:left + $(window).scrollLeft()
       });
     };
 
 
+    // Open the modal and center
     method.open = function (settings) {
       $content.empty().append(settings.content);
 
       $modal.css({
-          width: settings.width || 'auto', 
+          width: settings.width || 'auto',
           height: settings.height || 'auto'
       });
 
@@ -64,7 +70,8 @@
 
     function generateModal() {
 
-      // Generate the HTML and add it to the document
+      // Generate the HTML for modal, shaded overlay
+      // and add it to the document
       $overlay = $('<div id="overlay"></div>');
       $modal = $('<div id="modal"></div>');
       $content = $('<div id="content"></div>');
@@ -74,6 +81,7 @@
       $overlay.hide();
       $modal.append($content, $close);
 
+      //Closes modal when upper-right x is clicked
       $close.click(function(e){
         e.preventDefault();
         method.close();
@@ -82,22 +90,29 @@
     }
 
     $modal = generateModal();
-    $('body').append($overlay, $modal);                       
+    $('body').append($overlay, $modal);
     // Wait until the DOM has loaded before querying the document
     $.get('link_form.html', function(data){
       method.open({content: data});
       $modal.find('form').on('submit', function (e) {
         e.preventDefault();
+        //Set equal to the HTML form element
         var $form = $(this);
         $.ajax({
+          //This may be something to look at more to account for cross domain
+          //requests
+          beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader('Access-Control-Allow-Origin: http://localhost:8000');
+          },
           type: 'POST',
           url: $form.attr('action'),
           crossDomain: true,
+          //Sets data equal to a string of input elements
           data: $form.serialize(),
           dataType: 'json'
-        })
+        });
       });
     });
   }
 
-//})();
+})();
